8262b625c80b9bbfbd57a1284a482698b094f154
Andrea Di Lorenzo <andrea.dilorenzo07@gmail.com>
2021-01-27 11:11:17 +0100

    manca la propagazione

--- src/MatriceState.jsx
+++ src/MatriceState.jsx
@@ -34,14 +34,36 @@ class MatriceState extends Component {
     componentDidUpdate(){
         //let x = this.state.xState
         //let y = this.state.yState
-        //console.log('entra')
-        console.log(this.state.xState)
-        if( this.state.xState > 0 && this.state.xState < this.state.Righe -1 ){
+        console.log('xState,yState in cDupdate')
+        console.log(this.state.xState, this.state.yState)
+        // verifica che le x e le y siano all'interno della matrice e richiama la funzione clickStyle nel quadrato intorno
+        if( this.state.xState > 0 && this.state.xState < this.state.Righe -1 && this.state.yState > 0 && this.state.yState < this.state.Colonne - 1){
             if( this.state.matrice[this.state.xState-1][this.state.yState].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
                 this.clickStyle(this.state.xState-1,this.state.yState)
             }
             if( this.state.matrice[this.state.xState+1][this.state.yState].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
                 this.clickStyle(this.state.xState+1,this.state.yState)
+            }
+            if( this.state.matrice[this.state.xState-1][this.state.yState-1].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
+                this.clickStyle(this.state.xState-1,this.state.yState-1)
+            }
+            if( this.state.matrice[this.state.xState][this.state.yState-1].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
+                this.clickStyle(this.state.xState,this.state.yState-1)
+            }
+            if( this.state.matrice[this.state.xState+1][this.state.yState-1].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
+                this.clickStyle(this.state.xState+1,this.state.yState-1)
+            }
+            if( this.state.matrice[this.state.xState-1][this.state.yState+1].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
+                this.clickStyle(this.state.xState-1,this.state.yState+1)
+            }
+            if( this.state.matrice[this.state.xState][this.state.yState+1].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
+                this.clickStyle(this.state.xState,this.state.yState+1)
+               /*  this.setState({
+                    yState: this.state.yState+1
+                }) */
+            }
+            if( this.state.matrice[this.state.xState+1][this.state.yState+1].valore < this.state.matrice[this.state.xState][this.state.yState].valore){
+                this.clickStyle(this.state.xState+1,this.state.yState+1)
             }
         }
     }
@@ -55,67 +77,34 @@ class MatriceState extends Component {
                 matriceAp[i].push(this.state.matrice[i][j])
             }
         }
+        console.log('cordinate')
         console.log(x,y)
+        console.log('matriceAp')
+        console.log(matriceAp)
         if( x !== undefined && y !== undefined){
-            //if( this.state.count === 0){
-                if(x > -1 && x < this.state.Righe && y >-1 && y<this.state.Colonne){
-                    for(let i=x-1; i<x+2; i++){
-                        for(let j=y-1; j<y+2; j++){
-                            if(i> -1 && i < this.state.Righe && j>-1 && j<this.state.Colonne){
-                                if(matriceAp[i][j].valore < matriceAp[x][y].valore){
-                                    matriceAp[i][j].colore = true
-                                }
+            if(x > -1 && x < this.state.Righe && y > -1 && y < this.state.Colonne){
+                /* this.setState({
+                    xState: x,
+                    yState: y
+                }) */
+                for(let i=x-1; i<x+2; i++){
+                    for(let j=y-1; j<y+2; j++){
+                        if(i> -1 && i < this.state.Righe && j>-1 && j<this.state.Colonne){
+                            if(matriceAp[i][j].valore < matriceAp[x][y].valore){
+                                matriceAp[i][j].colore = true
                             }
                         }
                     }
-                    matriceAp[x][y].colore = true
-                    // salvi in set stase x e y in modo che il componentdiupdated richiama lo state x-1 e cosi via
-                    this.setState({
-                        matrice: [...matriceAp],
-                        xState: x,
-                        yState: y
-                    })
                 }
-            //}
-            /*if(x > -1 && x < this.state.Righe && y >-1 && y<this.state.Colonne){
-                if (this.state.count > 0 && matriceAp[x][y].colore === true){
-                    for(let i=x-1; i<x+2; i++){
-                        for(let j=y-1; j<y+2; j++){
-                            if(i> -1 && i < this.state.Righe && j>-1 && j<this.state.Colonne){
-                                if(matriceAp[i][j].valore < matriceAp[x][y].valore){
-                                    matriceAp[i][j].colore = true
-                                }
-                            }
-                        }
-                    }
-                    //matriceAp[x][y].colore = true
-                    // salvi in set stase x e y in modo che il componentdiupdated richiama lo state x-1 e cosi via
-                    this.setState({
-                        matrice: [...matriceAp],
-                        xState: x,
-                        yState: y,
-                        count: this.state.count +1
-                    })
-                }
-                else if (this.state.count > 0 && matriceAp[x][y].colore === false){
-                    console.log(x,y)
-                    for(let i=x-1; i<x+2; i++){
-                        for(let j=y-1; j<y+2; j++){
-                            if(i> -1 && i < this.state.Righe && j>-1 && j<this.state.Colonne){
-                                if(matriceAp[i][j].valore < matriceAp[x][y].valore){
-                                    matriceAp[i][j].colore = true
-                                }
-                            }
-                        }
-                    }
-                    matriceAp[x][y].colore = true
-                    // salvi in set stase x e y in modo che il componentdiupdated richiama lo state x-1 e cosi via
-                    this.setState({
-                        matrice: [...matriceAp],
-                        count: this.state.count +1
-                    })
-                }
-            }*/
+                matriceAp[x][y].colore = true
+                // salvo in set stase x e y in modo che il componentdiupdated richiama lo state x-1 e cosi via
+                console.log('set state')
+                this.setState({
+                    matrice: [...matriceAp],
+                    xState: x,
+                    yState: y
+                })
+            }
         }
     }
     
